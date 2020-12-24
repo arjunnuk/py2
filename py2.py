fdc6e51a24ece1a19b23c1076bb0a3115af36c99
Arjun Uk <61324768+arjunnuk@users.noreply.github.com>
2020-12-24 12:03:18 +0530

    Update py2.py

--- py2.py
+++ py2.py
@@ -12,6 +12,6 @@ for i in li:
         psum= psum+i
     else:
         nsum=nsum+i
-print("the sum positive numbers a is",psum)
+print("the sum positive numbers is",psum)
 print("the sum negative numbers is",nsum)
 print("arjunuk")
